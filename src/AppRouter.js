fcf87e19bd749526c11c09aedaa780a61e81412c
ritik7905 <ritik.trilok7905@gmail.com>
2022-11-06 20:00:50 +0530

    ADD: Sass_Setup_Again

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,34 +1,53 @@
-import { Layout } from 'antd'
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Layout } from "antd";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 // ===================Routes..===============
-import { About_Page, Blogs_Page, blog_Details, Cart_Page, Contact_Page, Home_Page, Page_Not_Found, product_Details, Shop_Page } from './common/routes'
+import {
+  About_Page,
+  Cart_Page,
+  Contact_Page,
+  Home_Page,
+  Page_Not_Found,
+  product_Details,
+  Shop_Page,
+} from "./common/routes";
 
 // ===================Pages===============
-import { About, BlogsPage, Cart, Contact, Home, PageNotFound, Shop, SingleBlogDetails, SingleProductDetails } from './pages'
-import { AppFooter, AppHeader } from './common'
+import {
+  About,
+  Cart,
+  Contact,
+  Home,
+  PageNotFound,
+  Shop,
+  SingleProductDetails,
+} from "./pages";
+import { AppFooter, AppHeader } from "./common";
 
 const { Content } = Layout;
 
 const AppRouter = () => {
-    return (
-        <Layout>
-            <AppHeader />
-            <Content>
-                <Routes>
-                    <Route exact path={Home_Page} element={<Home />} />
-                    <Route path={Shop_Page} element={<Shop />} />
-                    <Route path={`${product_Details}/:id`} element={<SingleProductDetails />} />
-                    <Route path={About_Page} element={<About />} />
-                    <Route path={Contact_Page} element={<Contact />} />
-                    <Route path={Cart_Page} element={<Cart />} />
-                    <Route path={Page_Not_Found} element={<PageNotFound />} />
-                </Routes>
-            </Content>
-            <AppFooter />
-        </Layout>
-    )
-}
+  return (
+    <Layout>
+      <AppHeader />
+      <Content>
+        <Routes>
+          <Route exact path={Home_Page} element={<Home />} />
+          <Route path={Shop_Page} element={<Shop />} />
+          <Route
+            path={`${product_Details}/:id`}
+            element={<SingleProductDetails />}
+          />
+          <Route path={About_Page} element={<About />} />
+          <Route path={Contact_Page} element={<Contact />} />
+          <Route path={Cart_Page} element={<Cart />} />
+          <Route path={Page_Not_Found} element={<PageNotFound />} />
+        </Routes>
+      </Content>
+      <AppFooter />
+    </Layout>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Card, Image } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Card, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const ProductCard = (props) => {
-    return (
-        <Card className='product-card'>
-            <Card.Body className='product-details'>
-                <Image fluid src={props.image_url} />
-            </Card.Body>
-            <Card.Title>
-                <Link to={"/"}>{props.dress_name}</Link>
-            </Card.Title>
-        </Card>
-    )
-}
+  return (
+    <Card className="product-card">
+      <Card.Body className="product-details">
+        <Image fluid src={props.image_url} />
+      </Card.Body>
+      <Card.Title>
+        <Link to={"/"}>{props.dress_name}</Link>
+      </Card.Title>
+    </Card>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
